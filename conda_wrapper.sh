c3624e6b9f6bfca413d1ec79c746500157a0fcf8
Udayan Gaikwad <ggwpfax@gmail.com>
2025-05-21 18:16:25 -0400

    code

--- conda_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate Conda environment and run the fine-tuning script
-
-eval "$(${HOME}/miniconda3/bin/conda shell.bash hook)"
-conda activate gpt2_finetune
-
-python finetune_gpt2.py
-  
